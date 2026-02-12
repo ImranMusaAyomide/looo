7f83b2c08832c7f8a7dc3590a6cb471b70f88eec
ImranMusaAyomide <haajmuskid@gmail.com>
2026-02-12 18:37:38 +0100

    Done chnages

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <Header />
       <HeroSection />
       <CardSection />
-  
+      <Summary />
     </div>
   )
 }
--- src/Summary.jsx
+++ src/Summary.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import './Summary.css'
+import Slack from './assets/noti.svg'
+import Notification from './assets/filter.svg'
 
 const Summary = () => {
   return (
-    <section>
-        <div className='side-left'>
-            <img src={Summary} alt="" />
-            <h1>AI Summary</h1>
-            <p></p>
-        </div>
+    <section className='to-middle'>
+    <div className='left' >
+      < img src={Slack} alt="" id='reduce'/>
+      <h1>Information Update</h1>
+      <p>Everything that need your attention
+      wrapped into one clean summary.</p>
+    </div> 
 
-    </section>
+    <div className='right'>
+      <img src={Notification} alt="" id='reduce'/>
+      <h1>Smart Filters </h1>
+      <p>ou most messages rise to the top so you always know what deserve your attewmtions first.</p>
+    </div>
+  </section>
   )
 }
 
